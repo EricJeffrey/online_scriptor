9fdaeb200939394021005ad3251d19cfa09e2317
EricJeffrey <1719937412@qq.com>
2022-06-09 19:14:12 +0800

    readme & config update

--- backend/config.hpp
+++ backend/config.hpp
@@ -11,8 +11,8 @@ constexpr char DB_PATH[] = "/data/online_scriptor/task_db";
 constexpr char PYTHON_PATH[] = "/usr/bin/python3";
 constexpr char BASH_PATH[] = "/usr/bin/bash";
 
-constexpr int PORT = 8000;
-constexpr int PORT_WS = 8001;
+constexpr int PORT = 6677;
+constexpr int PORT_WS = 6678;
 
 constexpr char STATIC_FILE_PREFIX[] = "/static/";
 constexpr char STATIC_FILE_DIR_PATH[] = "/data/online_scriptor/static/";
